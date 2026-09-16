e8ed07b8872f88a3b3f7f1f76bb407741177b601
Snehangsu Biswas <babai.biswas@gmail.com>
2026-09-16 17:57:24 +0530

    setup

--- main.go
+++ main.go
@@ -7,9 +7,36 @@ package main
 */
 import "C"
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func main() {
+func main(){
 	fmt.Println("llama.cpp C API loaded")
 	fmt.Printf("llama.cpp version: %s\n", C.GoString(C.llama_version()))
+	modelPath := "C:/Users/JAYANTA/Desktop/gguf_store/Swift-Qwen3.8-27B-Q4_K_M.gguf"
+
+	runModel(modelPath)
+}
+
+func runModel(modelPath string) error{
+
+	C.ggml_backend_load_all();
+	model_params := C.llama_model_default_params();
+    model_params.n_gpu_layers = C.int(99);
+
+	model := C.llama_model_load_from_file(C.CString(modelPath), model_params)
+	if(model == nil){
+		return errors.New("Model Not Found")
+	}
+
+	vocab := C.llama_model_get_vocab(model)
+	ctx_params := C.llama_context_default_params()
+
+	ctx := C.llama_init_from_model(model, ctx_params)
+	if(ctx == nil){
+		return errors.New("Could not set context")
+	}
+
 }
